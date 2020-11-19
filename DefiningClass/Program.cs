d01c4ecc0e027e03660f0c25ce8cd4da296e3488
Georgi Panaiotov <rippness@gmail.com>
2020-11-19 18:11:29 +0200

    Adding Class Exercises

---
+++ DefiningClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = Console.ReadLine();
+            car.Model = Console.ReadLine();
+            car.Year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
